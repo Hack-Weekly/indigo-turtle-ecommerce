d066f2940e08e7bb7cf68be0455c365917a0d64e
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-04-25 22:48:36 +0100

    progress being made

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,21 @@
 import React from "react";
-import "../styles.css"
+import "../../styles.css"
 
 export default function HomePage(): JSX.Element {
   return (
-  <span className="HomeTitle">Indigo E-Commerce</span>
+  <div>
 
+    <nav className="Home-nav">
+      <img className="Home-img"></img>
+      <span>About Us</span>
+      <span className="Home-title">Indigo E-Commerce</span>
+      <button className="Home-btn-signUp">Sign Up</button>
+    </nav>
 
+    
+  </div>
+  
+
+  
   )
 }
